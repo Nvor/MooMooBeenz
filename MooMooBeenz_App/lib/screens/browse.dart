cf1c6834e023a5bc74c4fc62ca4b5b5fd25e1de8
nvor <nickbaripour@gmail.com>
2021-04-21 22:22:10 -0400

    basic view user card

--- MooMooBeenz_App/lib/screens/browse.dart
+++ MooMooBeenz_App/lib/screens/browse.dart
@@ -1,3 +1,4 @@
+import 'package:MooMooBeenz_App/screens/view.dart';
 import 'package:flutter/material.dart';
 
 class BrowsePage extends StatefulWidget {
@@ -6,15 +7,48 @@ class BrowsePage extends StatefulWidget {
 }
 
 class _BrowsePageState extends State<BrowsePage> {
+  List userCards = new List.generate(20, (i) => UserCard()).toList();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Browse")
       ),
-      body: Center(
-        child: Text("Test Browse Page")
+      body: Container(
+        child: ListView(
+          children: [
+            UserCard(),
+            UserCard(),
+            UserCard()
+          ]
+        )
       )
+    );
+  }
+}
+
+class UserCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(30.0),
+            child: Row(
+                children: <Widget>[
+                  Icon(Icons.face),
+                  Text("Test Name")
+                ]
+          )
+        ),
+      ),
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => ViewPage())
+        );
+      }
     );
   }
 }
