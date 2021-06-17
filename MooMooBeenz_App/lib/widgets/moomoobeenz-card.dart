c68c8c7ee5d74a8e082ee2686d0199dec085ea02
nvor <nickbaripour@gmail.com>
2021-06-16 23:33:18 -0400

    moomoobeenz card PoC #1

--- MooMooBeenz_App/lib/widgets/moomoobeenz-card.dart
+++ MooMooBeenz_App/lib/widgets/moomoobeenz-card.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
 
-class MooMooBeenzCard extends StatelessWidget {
+class MooMooBeenzCard extends StatefulWidget {
+  @override
+  _MooMooBeenzCard createState() => _MooMooBeenzCard();
+}
+
+class _MooMooBeenzCard extends State<MooMooBeenzCard> {
+  List<bool> isSelected = [false, false, false, false, false];
+
   @override
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
-        padding: EdgeInsets.all(20.0),
-        child: Text("MooMooBeenz Card")
+        padding: EdgeInsets.all(15.0),
+        child: Center(child:ToggleButtons(
+          children: <Widget> [
+            Icon(Icons.favorite),
+            Icon(Icons.favorite),
+            Icon(Icons.favorite),
+            Icon(Icons.favorite),
+            Icon(Icons.favorite)
+          ],
+          selectedColor: Colors.white,
+          fillColor: Colors.black,
+          splashColor: Colors.black,
+          renderBorder: false,
+          onPressed: (int index) {
+            setState(() {
+              for (int buttonIndex = 0; buttonIndex < isSelected.length; buttonIndex++) {
+                if (buttonIndex == index) {
+                  isSelected[buttonIndex] = !isSelected[buttonIndex];
+                } else {
+                  isSelected[buttonIndex] = false;
+                }
+              }
+            });
+          },
+          isSelected: isSelected
+        ))
       )
     );
   }
